141c0a306930ad3ac36bb805581baebc5ca68803
Johnny Hausman <johnny@appdevdesigns.net>
2015-09-29 14:59:26 +0700

    +ADD: progress loading indications.

--- assets/opstools/HrisUserProfile/HrisUserProfile.js
+++ assets/opstools/HrisUserProfile/HrisUserProfile.js
@@ -1,8 +1,12 @@
 steal(
         // List your Page's dependencies here:
+        'appdev',
+        function() {
+            AD.ui.loading.resources(3);
+        },
         '//opstools/HrisUserProfile/controllers/HrisUserProfile.js'
         ,'//opstools/HrisUserProfile/hrisUserProfile.css'
         ,'site/labels/HRISUserProfile.js'
 ).then(function(){
-
+	AD.ui.loading.completed(3);
 });
